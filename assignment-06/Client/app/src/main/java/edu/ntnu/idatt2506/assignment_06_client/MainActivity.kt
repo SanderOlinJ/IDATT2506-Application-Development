55cac35be64490dcaee097fd789f4a7ea4782298
Sander Johansen <sander.olin@gmail.com>
2023-10-16 22:29:56 +0200

    Initiated assignment 6, Client and Host

---
+++ assignment-06/Client/app/src/main/java/edu/ntnu/idatt2506/assignment_06_client/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt2506.assignment_06_client
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ assignment-06/Host/app/src/main/java/edu/ntnu/idatt2506/assignment_06_host/MainActivity.kt
@@ -0,0 +1,21 @@
+package edu.ntnu.idatt2506.assignment_06_host
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ntnu.idatt2506.assignment_06_host.ui.theme.Assignment06HostTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
