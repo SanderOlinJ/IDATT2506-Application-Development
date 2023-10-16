d425629dec98d6228a1a13c2d8340d75b04e0ac3
Sander Johansen <sander.olin@gmail.com>
2023-10-16 20:08:20 +0200

    Finished assignment 5

--- assignment-05/app/src/main/java/edu/ntnu/idatt2506/assignment_05/MainActivity.kt
+++ assignment-05/app/src/main/java/edu/ntnu/idatt2506/assignment_05/MainActivity.kt
@@ -1,44 +1,54 @@
 package edu.ntnu.idatt2506.assignment_05
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import org.json.JSONArray
-
 
 const val URL = "https://bigdata.idi.ntnu.no/mobil/tallspill.jsp"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var startButton: Button
+    private lateinit var submitInfoButton: Button
     private lateinit var nameField: EditText
     private lateinit var cardNumberField: EditText
     private lateinit var errorField: TextView
+    private lateinit var numberGuess: EditText
+    private lateinit var submitGuessButton: Button
     private val network: HttpWrapper = HttpWrapper(URL)
+    private var messageFromServer: String = ""
+    private var numberOfGuesses: Int = 0
+    private var validInfo: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startButton = findViewById(R.id.start_playing_button)
+        submitInfoButton = findViewById(R.id.submit_info_button)
         nameField = findViewById(R.id.name)
         cardNumberField = findViewById(R.id.card_number)
         errorField = findViewById(R.id.error)
+        numberGuess = findViewById(R.id.number_guess)
+        submitGuessButton = findViewById(R.id.submit_guess_button)
+        val scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.button_scale_animation)
 
-        startButton.setOnClickListener {
-            if (checkValidInput()) {
-                performRequest(requestParameters())
-            }
+        submitInfoButton.setOnClickListener { v ->
+            submitInfo()
+            v.startAnimation(scaleAnimation)
+        }
+
+        submitGuessButton.setOnClickListener { v ->
+            submitGuess()
+            v.startAnimation(scaleAnimation)
         }
     }
 
@@ -46,55 +56,126 @@ class MainActivity : AppCompatActivity() {
         val name = nameField.text.toString()
         val cardNumber = cardNumberField.text.toString()
 
-        if (name.isEmpty() || cardNumber.isEmpty()) {
-            errorField.text = getString(R.string.empty_fields_error)
-            errorField.visibility = View.VISIBLE
-
-            val handler = Handler(Looper.getMainLooper())
-            handler.postDelayed({
-                errorField.visibility = View.GONE
-            }, 5000)
+        return if (name.isEmpty() || cardNumber.isEmpty()) {
+            showMessage(getString(R.string.empty_fields_error), true)
             Log.d("checkValidInput()","Validation fail!")
-
-            return false
+            false
         } else {
             errorField.visibility = View.GONE
             Log.d("checkValidInput()","Validation success!")
-
-            return true
+            true
         }
     }
 
-    private fun requestParameters(): Map<String, String> {
-        val name = nameField.text.toString()
+    private fun submitInfoParameters(): Map<String, String> {
+        val name = nameField.text.toString().replace(" ", "")
         val cardNumber = cardNumberField.text.toString()
+        Log.d("submitInfoParameters()", "Input name: $name")
+        Log.d("submitInfoParameters()", "Input card number: $cardNumber")
         return mapOf(
             "navn" to name,
             "kortnummer" to cardNumber,
         )
     }
 
-    private fun performRequest(parameterList: Map<String, String>) {
+    private fun submitGuessParameters(): Map<String, String> {
+        val guess = numberGuess.text.toString()
+        Log.d("submitGuessParameters()", "User's guess: $guess")
+        return mapOf(
+            "tall" to guess
+        )
+    }
+
+    private fun performRequest(parameterList: Map<String, String>, callback: (String?) -> Unit) {
         CoroutineScope(Dispatchers.IO).launch {
             val response: String = try {
                 network.get(parameterList)
             } catch (e: Exception) {
-                Log.e("performRequest()", e.message!!)
+                Log.e("performRequest()", "Error from server:\n$e.message!!")
                 e.toString()
             }
-            Log.d("performRequest()", response)
+            Log.d("performRequest()", "Response from server:\n$response")
+            messageFromServer = response
             MainScope().launch {
-                errorField.text = formatJsonString(response)
+                messageFromServer = response
+                callback(response)
             }
         }
     }
 
-    private fun formatJsonString(str: String): String {
-        return try {
-            JSONArray(str).toString(4)
-        } catch (e: Exception) {
-            Log.e("formatJsonString()", e.toString())
-            e.message!!
+    private fun submitInfo() {
+        clearMessage()
+        if (!checkValidInput()) {
+            return
+        }
+        performRequest(submitInfoParameters()) { response ->
+            if (response != null) {
+                val trimmedResponse = response.replace("null", "").trim()
+                if (response.contains("Feil", ignoreCase = true)) {
+                    Log.d("submitInfo()", "Response contains 'Feil': $response")
+                    showMessage(trimmedResponse, true)
+                    validInfo = false
+                } else {
+                    numberOfGuesses = 0
+                    validInfo = true
+                    showMessage(trimmedResponse, false)
+                    Log.d("submitInfo()", "Received response from server: $response")
+                }
+            } else {
+                Log.d("submitInfo()", "Failed to receive response from server")
+            }
+        }
+    }
+
+    private fun submitGuess() {
+        clearMessage()
+        if (!validInfo) {
+            showMessage(getString(R.string.invalid_info), true)
+            Log.d("submitGuess()","Validation fail!")
+            return
+        }
+        if (numberOfGuesses >= 3) {
+            showMessage(getString(R.string.exceeded_guesses), true)
+            Log.d("submitGuess()","Validation fail!")
+            validInfo = false
+            return
+        }
+
+        performRequest(submitGuessParameters()) { response ->
+            if (response == null) {
+                Log.d("submitGuess()", "Failed to receive response from server")
+                return@performRequest
+            }
+
+            if (response.contains("Tallet er ikke på riktig form")) {
+                showMessage(getString(R.string.wrong_guess_format), true)
+                Log.d("submitGuess()","Validation fail!")
+                return@performRequest
+            }
+
+            val trimmedResponse = response.replace("null", "").trim()
+            showMessage(trimmedResponse, false)
+
+            numberOfGuesses++
+            Log.d("submitGuess()", "Received response from server: $response")
+        }
+    }
+
+    private fun clearMessage() {
+        errorField.text = ""
+        errorField.visibility = View.GONE
+    }
+
+    private fun showMessage(message: String, error: Boolean) {
+        errorField.text = message
+        errorField.visibility = View.VISIBLE
+
+        if (error) {
+            val textColor = ContextCompat.getColor(this, R.color.red)
+            errorField.setTextColor(textColor)
+        } else {
+            val textColor = ContextCompat.getColor(this, R.color.green)
+            errorField.setTextColor(textColor)
         }
     }
 }
